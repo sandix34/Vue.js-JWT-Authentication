22e3aed67ea6cb799eb9682a289b055ee15239e2
sandrine <sandrinezocca@gmail.com>
2020-02-10 17:23:25 +0100

    📐  header implementation

--- client/src/App.vue
+++ client/src/App.vue
@@ -24,6 +24,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
--- client/src/components/TheHeader.vue
+++ client/src/components/TheHeader.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Header</div>
+  <div class="container-header d-flex w-100 flex-row justify-content-center align-items-center p-3">
+    <router-link to="/">Vue-JWT</router-link>
+    <div class="flex-fill"></div>
+    <div class="d-flex flex-row">
+      <router-link to="signin" class="mr-2">Connexion</router-link>
+      <router-link to="signup">Inscription</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,5 +19,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.flex-fill {
+  flex: auto;
+}
+.container-header {
+  background-color: #273c75;
+  color: white;
+}
+a {
+  color: white;
+}
 </style>
